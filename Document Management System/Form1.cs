a9226728e46aaaa7ce87fe1badc5f9d55a58ce2d
Evan Crabtree <ecrab21@gmail.com>
2020-01-09 09:34:54 +0000

    Add files via upload

--- Document Management System/Form1.cs
+++ Document Management System/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,13 @@ namespace Document_Management_System
         public Form1()
         {
             InitializeComponent();
+
+            string connectionStr = "";
+
+            using (SqlConnection con = new SqlConnection(connectionStr))
+            {
+
+            }
         }
     }
 }
