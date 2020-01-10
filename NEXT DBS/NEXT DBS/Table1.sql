4457200db7041bcfca15151d17ccaa077eccbbe8
Flemmingben <58795402+Flemmingben@users.noreply.github.com>
2020-01-10 13:35:51 +0000

    Add files via upload

---
+++ NEXT DBS/NEXT DBS/Table1.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Table1]
+(
+	[DocId] INT NOT NULL PRIMARY KEY,
+	[DocName] VARCHAR(20) NOT NULL,
+	[Edit Date] DATE NOT NULL,
+	[Author] VARCHAR(15) NOT NULL,
+	[Rating] DECIMAL(2, 1) DEFAULT 0,
+	[Tags] VARCHAR(10) 
+
+
+)
