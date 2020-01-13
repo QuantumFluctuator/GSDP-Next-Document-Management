bfdbb53dbb70378074654f32943e4edfd551bdaa
Evan Crabtree <ecrab21@gmail.com>
2020-01-13 21:55:02 +0000

    Add files via upload

--- Website assignment/index.php
+++ Website assignment/index.php
@@ -1,6 +1,13 @@
 <!doctype html>
 <html>
+    <?php
+    $connect = mysqli_connect("localhost","next", "nextTeam2","nextDocumentManager");
 
+    if (!$connect) {
+        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+        die(mysqli_error());
+    }
+    ?>
     <head>
         <meta charset="utf-8">
         <title>NEXT Document Manager</title>
@@ -16,9 +23,31 @@
                     </div>
             </header>
 
-            <!--Button to take user to login form !-->
-            <button type="submit" onclick="location.href='login.php' ">Sign in</button>
+            <div class = "buttons">
+                <!--Button to take user to login form !-->
+                <button type="submit" onclick="location.href='contact-me.html' ">Sign in</button>
 
+                 <div class = "upload">
+                    <form action="upload.php" method="post" enctype="multipart/form-data">
+                        <!--Select image to upload: !-->
+                        <button type="file" name="fileToUpload" id="fileToUpload"> Upload Document </button>
+
+                    </form>
+                </div>
+            </div>
+        </div>
+            
+        <div class="topnav">
+            <form action="index.php" method="post" align="center">
+                <input type="text" name="search" placeholder="Search" />
+                <input type="submit" value="Go" />
+            </form>
+
+
+            <?php // outputting search results on vutton click
+            print("$output");
+            ?>
+        </div>
 
         <?php
         //Collect
@@ -28,53 +57,24 @@
             $searchq = preg_replace("#[^0-9 a-z . _ ]#i","",$searchq);
 
             //SQL query, comparing search string to column data
-            $query = mysql_query("SELECT * FROM documents WHERE ID LIKE '%$searchq%' OR Name LIKE '%$searchq%' OR Location LIKE '%$searchq%' Or LastModified LIKE '%$searchq%' Or Rating LIKE '%$searchq%' Or Author LIKE '%$searchq%'") or die ("Could not search.");
+            $query = mysqli_query($connect, "SELECT * FROM documents WHERE (Name LIKE '%.doc' OR Name LIKE '%.docx' OR Name LIKE '%.txt') AND (Name LIKE '%$searchq%' OR ID LIKE '%$searchq%' OR Location LIKE '%$searchq%' OR LastModified LIKE '%$searchq%' OR Rating LIKE '%$searchq%' OR Author LIKE '%$searchq%')"); // ID LIKE '%$searchq%' OR Name LIKE '%$searchq%' OR Location LIKE '%$searchq%' OR LastModified LIKE '%$searchq%' OR Rating LIKE '%$searchq%' OR Author LIKE '%$searchq%'");// or die ("Could not search.");
 
+            $count = mysqli_num_rows($query);
+            
             //if no matches to the search
             if($count == 0){
                 $output = 'There were no results for your search.';
 
             }else{
-                //While loop to collect relevant information from the rows
-                while($row = mysql_fetch_array($query)){
-                    $docID = $row['ID'];
-                    $docName = $row['Name'];
-                    $loc = $row['Location'];
-                    $moddate = $row['LastModified'];
-                    $rated = $row['Rating'];
-                    $author = $row['Author'];
-
-                    $output .= '<div>'.$docID.' '.$docName.' '.$loc.' '.$moddate.' '.$rated.' '.$author.'</div>';
-
-                }
-            }   
-        }
-        }
-        ?>
-
-        <div class="topnav">
-            <form action="index.php" method="post">
-                <input type="text" name="search" placeholder="Search" />
-                <input type="submit" value="Go" />
-            </form>
-
-
-        <?php // outputting search results on button click
-        print("$output");?>
-
-        </div>
-
-
-
-
-            <hr size="6" width="75%" align="left" color="black">
-
+            ?>
+            <hr size="6" width="100%" align="center" color="black">
+            <h2>Search Results</h2>
             <table>
                 <thead>
                     <tr>
                         <td>Document ID</td>
-                        <td>File Location</td>
                         <td>Document Name</td>
+                        <td>File Location</td>
                         <td>Last Edited</td>
                         <td>Rating</td>
                         <td>Author</td>
@@ -82,37 +82,74 @@
                     </tr>
                 </thead>
                 <tbody>
+            
                 <?php
-                    $connect = mysqli_connect("localhost","next", "nextTeam2","nextDocumentManager");
-                    if (!$connect) {
-                        echo "Failed to connect to MySQL: " . mysqli_connect_error();
-                        die(mysqli_error());
-                    }
-                    
-                    $results = mysqli_query($connect, "SELECT * FROM documents");
-                    
-                    if (!$results) {
-                        $message  = 'Invalid query: ' . mysqli_error() . "\n";
-                        die($message);
-                    }
-                    
-                    while($row = mysqli_fetch_assoc($results)) {
-                    ?>
-                        <tr>
-                            <td><?php echo $row['ID']?></td>
-                            <td><?php echo $row['Location']?></td>
-                            <td><?php echo "<a href=Documents/" . str_replace(' ', '%20', $row['Location']) . str_replace(' ', '%20', $row['Name']) . " download>" . $row['Name'] . "</a>"?></td>
-                            <td><?php echo $row['LastModified']?></td>
-                            <td><?php echo $row['Rating']?></td>
-                            <td><?php echo $row['Author']?></td>
+                
+                //While loop to collect relevant information from the rows
+                while($row = mysqli_fetch_assoc($query)){
+                ?>
+                    <tr>
+                            <td><?php echo str_ireplace($searchq, "<mark>" . $searchq . "</mark>", $row['ID'])?></td>
+                            <td><?php echo "<a href=Documents/" . str_replace(' ', '%20', $row['Location']) . str_ireplace(' ', '%20', $row['Name']) . " download>" . str_ireplace($searchq, "<mark>" . $searchq . "</mark>", $row['Name']) . "</a>"?></td>
+                            <td><?php echo str_ireplace($searchq, "<mark>" . $searchq . "</mark>", $row['Location'])?></td>
+                            <td><?php echo str_ireplace($searchq, "<mark>" . $searchq . "</mark>", $row['LastModified'])?></td>
+                            <td><?php echo str_ireplace($searchq, "<mark>" . $searchq . "</mark>", $row['Rating'])?></td>
+                            <td><?php echo str_ireplace($searchq, "<mark>" . $searchq . "</mark>", $row['Author'])?></td>
                             <td></td>
-                        </tr>
-                        
+                    </tr>
+                <?php
+                }
+                ?>
                 </tbody>
             </table>
-            <footer>
-                    <p>All work copyright &copy; of Ben Flemming, Zak Edwards, Evan Crabtree, Declan Eagle 2020</p>
-            </footer>
-        </div>
+                <?php
+            }   
+        }
+        ?>
+        <hr size="6" width="100%" align="center" color="black">
+        <h2>All Documents</h2>
+        <table>
+            <thead>
+                <tr>
+                    <td>Document ID</td>
+                    <td>Document Name</td>
+                    <td>File Location</td>
+                    <td>Last Edited</td>
+                    <td>Rating</td>
+                    <td>Author</td>
+                    <td>Tags</td>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                $results = mysqli_query($connect, "SELECT * FROM documents WHERE Name LIKE '%.doc' OR Name LIKE '%.docx' OR Name LIKE '%.txt'");
+
+                if (!$results) {
+                    $message  = 'Invalid query: ' . mysqli_error() . "\n";
+                    die($message);
+                }
+
+                while($row = mysqli_fetch_assoc($results)) {
+                ?>
+                    <tr>
+                        <td><?php echo $row['ID']?></td>
+                        <td><?php echo "<a href=Documents/" . str_replace(' ', '%20', $row['Location']) . str_replace(' ', '%20', $row['Name']) . " download>" . $row['Name'] . "</a>"?></td>
+                        <td><?php echo $row['Location']?></td>
+                        <td><?php echo $row['LastModified']?></td>
+                        <td><?php echo $row['Rating']?></td>
+                        <td><?php echo $row['Author']?></td>
+                        <td></td>
+                    </tr>
+                <?php
+                }
+                ?>
+            </tbody>
+        </table>
+
+        <hr size="6" width="100%" align="center" color="black">
+
+        <footer>
+                <p>All work copyright &copy; of Ben Flemming, Zak Edwards, Evan Crabtree, Declan Eagle 2020</p>
+        </footer>
     </body>
 </html>
