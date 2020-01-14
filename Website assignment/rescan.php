94e07fc1db4097b7ef35067d686234d6194e1e73
Evan Crabtree <ecrab21@gmail.com>
2020-01-14 00:23:04 +0000

    Add files via upload

--- Website assignment/rescan.php
+++ Website assignment/rescan.php
@@ -11,19 +11,58 @@
             foreach($files as $key => $value){
                 $path = realpath($dir.DIRECTORY_SEPARATOR.$value);
                 if(!is_dir($path)) {
-                    $results[] = $path;
+                    $results[] = str_replace("/var/www/html/DocumentManager/Documents/", "", $path);
                 } else if($value != "." && $value != "..") {
                     getDirContents($path, $results);
-                    $results[] = $path;
+                    //$results[] = str_replace("/var/www/html/DocumentManager/Documents/", "", $path);
                 }
             }
 
             return $results;
         }
+        
+        $results = array();
+        $results = getDirContents('Documents/');
+        
+        //var_dump(getDirContents('Documents/'));
+        
+        $connect = mysqli_connect("localhost","next", "nextTeam2","nextDocumentManager");
 
-        var_dump(getDirContents('Documents/'));
+        if (!$connect) {
+            echo "Failed to connect to MySQL: " . mysqli_connect_error();
+            die(mysqli_error());
+        }
+        
+        $i = 0;
+        
+        //print_r($results);
+        
+        /*$delete = mysqli_query($connect, "DELETE FROM documents WHERE 1");
 
-        echo $results;
+        if ($connect->query($delete) === TRUE) {
+            echo "<br>Table cleared successfully";
+        } else {
+            echo "Error: " . $sql . "<br>" . $connect->error;
+        }*/
+        
+        while ($i <= count($results)) {
+            $name = basename($results[$i]);
+            $loc = str_replace($name, "", $results[$i]);
+            
+            $id = $i;
+            
+            $insert = mysqli_query($connect, "INSERT INTO documents (ID, Name, Location, Approved) VALUES ($id, '$name', '$loc', FALSE)");
+
+            if ($connect->query($insert) === TRUE) {
+                echo "<br>New record created successfully";
+            } else {
+                echo "<br>Error: " . $sql . "<br>" . $connect->error;
+            }
+        
+            $i++;
+        }
+        
+        $connect->close();
         ?>
     </body>
 </html>
