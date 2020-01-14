88e9d527e5f04840ea58cf453c929a3c6ee15d28
Evan Crabtree <ecrab21@gmail.com>
2020-01-14 18:44:52 +0000

    Add files via upload

--- Website assignment/rate.php
+++ Website assignment/rate.php
@@ -1,7 +1,9 @@
 <!doctype html>
 <html>
     <head>
+        <meta charset="utf-8">
         <title>Rate Document</title>
+        <link href="css/main.css" rel="stylesheet" type="text/css">
     </head>
     
     <body>
