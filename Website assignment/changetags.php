74f8340450330e4ef294c9b5e525bc4ba24d4fd8
DeclanEagle <48287082+DeclanEagle@users.noreply.github.com>
2020-01-15 13:27:36 +0000

    Add files via upload

--- Website assignment/changetags.php
+++ Website assignment/changetags.php
@@ -5,7 +5,16 @@
         <title>Edit Tags</title>
         <link href="css/main.css" rel="stylesheet" type="text/css">
     </head>
-    
+
+    <?php
+    $connect = mysqli_connect("localhost","next", "nextTeam2","nextDocumentManager");
+
+    if (!$connect) {
+        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+        die(mysqli_error());
+    }
+    ?>
+
     <body>
         <div id="container">
             <header>
@@ -14,6 +23,66 @@
                 </div>
             </header>
 
+
+    <form>
+    <fieldset>
+    <legend>Edit Tags:</legend>
+    <label>Enter a document ID:</label>
+    <p>
+        <input type="text" name="ID"/>
+    </p>
+    <label>Add a new tag ID:</label>
+    <p>
+        <input type="text" name="Tag ID"/>
+    </p>
+
+    <!--<form action= "script.php" method = "get" >
+    <input type= "submit" value= "Submit" > -->
+
+    <?php
+    if (!empty($_POST['act'])) 
+    {
+    $id= $_POST['ID']; // saves description enterd 
+    $tagid= $_POST['Tag ID']
+    
+    $insert = mysqli_query($connect, "INSERT INTO TagLink (ID, TagID) VALUES ($id, $tagid)");
+
+            if ($connect->query($insert) === TRUE) {
+                echo "<br>New tag added successfully";
+            } else {
+                echo "<br>Error: " . $sql . "<br>" . $connect->error;
+            }
+
+    } else {
+    ?>
+
+    <form action="changetags.php" method="post">
+    <input type="hidden" name="act" value="run">
+    <input type="submit" value="Run me now!">
+    </form>
+
+    <?php
+    }
+    ?>
+
+    <p><label>Tag ID:</label></p>
+    <p><label>1 Support</label></p>
+    <p><label>2 Architectural</label></p>
+    <p><label>3 User</label></p>
+    <p><label>4 Analysis</label></p>
+    <p><label>5 Design</label></p>
+    <p><label>6 Testing</label></p>
+    <p><label>7 Results</label></p>
+    <p><label>8 Framework</label></p>
+    <p><label>9 Planning</label></p>
+    <p><label>10 .docx</label></p>
+    <p><label>11 .doc</label></p>
+    <p><label>12 .txt</label></p>
+
+    
+        
+    </fieldset>
+    </form>
             <footer>
                 <p>All work copyright &copy; of Ben Flemming, Zak Edwards, Evan Crabtree, Declan Eagle 2020</p>
             </footer>
