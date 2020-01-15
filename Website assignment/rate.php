12d27ca8043a027fe7293827127e546cef91f4e0
ZakEdwards15 <48760764+ZakEdwards15@users.noreply.github.com>
2020-01-15 16:26:04 +0000

    Add files via upload
    Rate.php
    Not tested yet

--- Website assignment/rate.php
+++ Website assignment/rate.php
@@ -2,10 +2,10 @@
 <html>
     <head>
         <meta charset="utf-8">
-        <title>Rate Document</title>
+        <title>Add Ratings</title>
         <link href="css/main.css" rel="stylesheet" type="text/css">
     </head>
-    
+
     <body>
         <div id="container">
             <header>
@@ -13,7 +13,62 @@
                     <h1><a href="index.php">NEXT DOCUMENT MANAGER</a></h1>
                 </div>
             </header>
+            <form>
+                <fieldset>
+                    <legend>Add Rating:</legend>
+                    <label>Enter a document ID:</label>
+                    <p>
+                        <input type="text" name="DocID"/>
+                    </p>
+                    <label>Enter a rating 1-5:</label>
+                    <p>
+                        <input type="text" name="Rating"/>
+                    </p>
 
+                    <!--<form action= "script.php" method = "get" >
+                    <input type= "submit" value= "Submit" > -->
+
+                    <?php
+                    if (!empty($_GET['act'])) 
+                    {
+                        $connect = mysqli_connect("localhost","next", "nextTeam2","nextDocumentManager");
+                        if (!$connect) {
+                            echo "Failed to connect to MySQL: " . mysqli_connect_error();
+                            die(mysqli_error());
+                        }
+                        $id= $_GET['DocID']; // saves description enterd 
+                        $rating= $_GET['Rating'];
+                        $MaxID = mysqli_query($connect, "SELECT COUNT(ID) FROM documents");
+                        if($id > $MaxID){
+                            echo "Enter a ID Below".($MaxID);
+                        }
+                        if($rating  < 0 ) || ($rating > 5){
+                            echo "Enter a value between 1 and 5";
+                        }
+                        while(($rating  > 0 ) || ($rating < 6) && ($id <= $MaxID)){
+                            //$insert = mysqli_query($connect, "INSERT INTO TagLink (ID, TagID) VALUES ($id, $tagid)");
+                            // Need to add user when login is added 
+                            $insert = mysqli_query($connect,"INSERT INTO Rating (RatingValue, DocumentID) VALUES ($rating, $id)");
+                            if ($connect->query($insert) === TRUE) {
+                                echo "<br>New Rating added successfully";
+                            }
+                            else{
+                                echo "<br>Rating coundnt be added"
+
+                            }
+                        }
+                    }  
+
+                    ?>
+
+                    <form action="rate.php" method="get">
+                        <input type="hidden" name="act" value="run">
+                        <input type="submit" value="Submit">
+                    </form>
+                    }
+
+                </fieldset>
+            </form>
             <footer>
                 <p>All work copyright &copy; of Ben Flemming, Zak Edwards, Evan Crabtree, Declan Eagle 2020</p>
             </footer>
