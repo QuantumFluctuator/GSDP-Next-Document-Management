eea31610e20f5d42ad51e7860bae79206b60a7ce
Flemmingben <58795402+Flemmingben@users.noreply.github.com>
2020-01-16 14:50:16 +0000

    Add files via upload

--- Website assignment/login.php
+++ Website assignment/login.php
@@ -1,3 +1,4 @@
+<?php include('functions.php') ?>
 <!doctype html>
 <html>
     <head>
@@ -6,15 +7,17 @@
         <link href="css/main.css" rel="stylesheet" type="text/css">
     </head>
     <body>
-        <div class="container">
+        <div id="container">
             <header>
                 <div id="logo">
                     <h1><a href="index.php">NEXT DOCUMENT MANAGER</a></h1>
                 </div>
             </header>
 
-            <form>
-                <div class="container">
+            <form method="post" action="login.php">
+                <?php echo display_error(); ?>
+
+                <div class="input-group">
                     <label for="uname"><b>Username</b></label>
                     <input type="text" placeholder="Enter Username" name="uname" required>
 
@@ -31,7 +34,7 @@
                     <button type="button" class="cancelbtn" onclick="location.href='index.php'">Cancel</button>
                 </div>
             </form>
-            
+
             <footer>
                 <p>All work copyright &copy; of Ben Flemming, Zak Edwards, Evan Crabtree, Declan Eagle 2020</p>
             </footer>
