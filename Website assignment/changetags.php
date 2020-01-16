e23300123bbfe41cc944c4056d9cb1bf1dab9f4d
Evan Crabtree <ecrab21@gmail.com>
2020-01-16 02:48:36 +0000

    Appearance fixes

--- Website assignment/changetags.php
+++ Website assignment/changetags.php
@@ -16,60 +16,54 @@
 
 
             <form>
-                <fieldset>
-                    <legend>Edit Tags:</legend>
-                    <label>Enter a document ID:</label>
-                    <p>
-                        <input type="text" name="DocID"/>
-                    </p>
-                    <label>Add a new tag ID:</label>
-                    <p>
-                        <input type="text" name="TagID"/>
-                    </p>
-                    
-                    <?php
-                    if (!empty($_GET['act'])) 
-                    {
-                        $connect = mysqli_connect("localhost","next", "nextTeam2","nextDocumentManager");
-                        if (!$connect) {
-                            echo "Failed to connect to MySQL: " . mysqli_connect_error();
-                            die(mysqli_error());
-                        }
-                        $id= $_GET['DocID']; // saves description enterd 
-                        $tagid= $_GET['TagID'];
-                        $insert = mysqli_query($connect, "INSERT INTO TagLink (ID, TagID) VALUES ($id, $tagid)");
-                        if ($connect->query($insert) === TRUE) {
-                            echo "<br>New tag added successfully";
-                        }
+                <legend><h3>Edit Tags</h3></legend>
+                <label>Enter a document ID:</label>
+                <input type="text" name="DocID"/>
+                <label>Add a new tag ID:</label>
+                <input type="text" name="TagID"/>
+
+                <?php
+                if (!empty($_GET['act'])) 
+                {
+                    $connect = mysqli_connect("localhost","next", "nextTeam2","nextDocumentManager");
+                    if (!$connect) {
+                        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+                        die(mysqli_error());
                     }
-                    ?>
+                    $id= $_GET['DocID']; // saves description enterd 
+                    $tagid= $_GET['TagID'];
+                    $insert = mysqli_query($connect, "INSERT INTO TagLink (ID, TagID) VALUES ($id, $tagid)");
+                    if ($connect->query($insert) === TRUE) {
+                        echo "<br>New tag added successfully";
+                    }
+                }
+                ?>
 
-                    <form action="changetags.php" method="get">
-                        <input type="hidden" name="act" value="run">
-                        <input type="submit" value="Submit" class="input">
-                    </form>
+                <form action="changetags.php" method="get">
+                    <input type="hidden" name="act" value="run">
+                    <input type="submit" value="Submit" class="input">
+                </form>
 
-                    <table>
-                        <thead>
-                            <tr>
-                                <th>ID</th>
-                                <th>Tag</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <?php
-                            $connect = mysqli_connect("localhost","next", "nextTeam2","nextDocumentManager");
-                            $results = mysqli_query($connect, "SELECT * FROM Tag") or die ("could not connect");
-                            while($row = mysqli_fetch_assoc($results)) {
-                                echo "<tr>";
-                                echo "<td>".$row['TagID']."</td>";
-                                echo "<td>".$row['TagName']."</td>";
-                                echo "</tr>";
-                            }
-                            ?>
-                        </tbody>
-                    </table>
-                </fieldset>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Tag</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        $connect = mysqli_connect("localhost","next", "nextTeam2","nextDocumentManager");
+                        $results = mysqli_query($connect, "SELECT * FROM Tag") or die ("could not connect");
+                        while($row = mysqli_fetch_assoc($results)) {
+                            echo "<tr>";
+                            echo "<td>".$row['TagID']."</td>";
+                            echo "<td>".$row['TagName']."</td>";
+                            echo "</tr>";
+                        }
+                        ?>
+                    </tbody>
+                </table>
             </form>
             <footer>
                 <p>All work copyright &copy; of Ben Flemming, Zak Edwards, Evan Crabtree, Declan Eagle 2020</p>
--- Website assignment/rate.php
+++ Website assignment/rate.php
@@ -14,50 +14,42 @@
                 </div>
             </header>
             <form>
-                <fieldset>
-                    <legend>Add Rating:</legend>
-                    <label>Enter a document ID:</label>
-                    <p>
-                        <input type="text" name="DocID"/>
-                    </p>
-                    <label>Enter a rating 1-5:</label>
-                    <p>
-                        <input type="text" name="Rating"/>
-                    </p>
+                <legend><h3>Add Rating</h3></legend>
+                <label>Enter a document ID:</label>
+                <input type="text" name="DocID"/>
+                <label>Enter a rating 1-5:</label>
+                <input type="text" name="Rating"/>
 
-                    <!--<form action= "script.php" method = "get" >
-                    <input type= "submit" value= "Submit" > -->
-
-                    <?php
-                    if (!empty($_GET['act'])) 
-                    {
-                        $connect = mysqli_connect("localhost","next", "nextTeam2","nextDocumentManager");
-                        if (!$connect) {
-                            echo "Failed to connect to MySQL: " . mysqli_connect_error();
-                            die(mysqli_error());
-                        }
-                        $id= $_GET['DocID']; // saves description enterd 
-                        $rating= $_GET['Rating'];
-                        $MaxID = mysqli_query($connect, "SELECT COUNT(ID) FROM documents");
-                        if($id > $MaxID){
-                            echo "Enter a ID Below".($MaxID);
-                        }
-                        if (($rating  < 0 ) || ($rating > 5)) {
-                            echo "Enter a value between 1 and 5";
-                        }
-                        if (($rating  > 0 ) || ($rating < 6) && ($id <= $MaxID)){
-                            // TODO: Need to add user when login is added 
-                            $insert = mysqli_query($connect,"INSERT INTO Rating (RatingValue, DocumentID) VALUES ($rating, $id)");
-                        }
+                <?php
+                if (!empty($_GET['act'])) 
+                {
+                    $connect = mysqli_connect("localhost","next", "nextTeam2","nextDocumentManager");
+                    if (!$connect) {
+                        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+                        die(mysqli_error());
                     }
-                    ?>
+                    $id= $_GET['DocID']; // saves description enterd 
+                    $rating= $_GET['Rating'];
+                    $MaxID = mysqli_query($connect, "SELECT COUNT(ID) FROM documents");
+                    if($id > $MaxID){
+                        echo "Enter a ID Below".($MaxID);
+                    }
+                    if (($rating  < 0 ) || ($rating > 5)) {
+                        echo "Enter a value between 1 and 5";
+                    }
+                    if (($rating  > 0 ) || ($rating < 6) && ($id <= $MaxID)){
+                        // TODO: Need to add user when login is added 
+                        $insert = mysqli_query($connect,"INSERT INTO Rating (RatingValue, DocumentID) VALUES ($rating, $id)");
+                    }
+                }
+                ?>
 
-                    <form action="rate.php" method="get">
-                        <input type="hidden" name="act" value="run">
-                        <input type="submit" value="Submit" class="input">
-                    </form>
+                <form action="rate.php" method="get">
+                    <input type="hidden" name="act" value="run">
+                    <input type="submit" value="Submit" class="input">
+                </form>
 
-                </fieldset>
+
             </form>
             <footer>
                 <p>All work copyright &copy; of Ben Flemming, Zak Edwards, Evan Crabtree, Declan Eagle 2020</p>
